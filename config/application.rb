1dabeb2650d8ae1761eec3735efae82ec3b03e8c
廣田真由 <my-hrt@i.softbank.jp>
2019-08-08 16:44:42 +0900

    application.rb の設定変更

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,21 @@ module QiitaClone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+        fixtures: true,
+        fixture_replacement: :factory_bot,
+        view_specs: false,
+        routing_specs: false,
+        helper_specs: false,
+        controller_specs: false,
+        request_specs: true
+    end
+    config.api_only = true
+    config.middleware.use ActionDispatch::Flash
   end
 end
