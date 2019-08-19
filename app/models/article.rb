52d7a609c3d730b7682871aa00115dfc5707351b
廣田真由 <my-hrt@i.softbank.jp>
2019-08-19 17:48:32 +0900

    dependent: :destroy 追記

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,6 +12,6 @@
 
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :article_likes
-  has_many :comments
+  has_many :article_likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :articles
-  has_many :article_likes
-  has_many :comments
+  has_many :articles, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
