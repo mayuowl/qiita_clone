82af5c2cea61c3f1b6c9498aa3f7890bef19e4ea
廣田真由 <my-hrt@i.softbank.jp>
2019-09-04 15:41:10 +0900

    update/destroyメソッド追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,29 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  before_action :set_user, only:[:show, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
+
+  def update
+    @article.update!(article_params)
+  end
+
+  def destroy
+    @article.destroy!
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+    def set_user
+      @article = Article.find(params[:id])
+    end
 end
