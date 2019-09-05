8ba821f51fd0ff878f5ee0a0c3192319490e53b2
廣田真由 <my-hrt@i.softbank.jp>
2019-09-05 15:00:55 +0900

    テスト describe を追加修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -15,22 +15,26 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  context "タイトルと本文が入力されている時" do
-    it "登録出来る" do
-      article = create(:article)
-      expect(article).to be_valid
+  describe "正常系" do
+    context "タイトルと本文が入力されている時" do
+      it "登録出来る" do
+        article = create(:article)
+        expect(article).to be_valid
+      end
     end
   end
-  context "タイトルが入力されていない時" do
-    it "登録出来ない" do
-      article = build(:article, title: nil)
-      expect(article.valid?).to be false
+  describe "異常系" do
+    context "タイトルが入力されていない時" do
+      it "登録出来ない" do
+        article = build(:article, title: nil)
+        expect(article.valid?).to be false
+      end
     end
-  end
-  context "本文が入力されていない時" do
-    it "登録出来ない" do
-      article = build(:article, body: nil)
-      expect(article.valid?).to be false
+    context "本文が入力されていない時" do
+      it "登録出来ない" do
+        article = build(:article, body: nil)
+        expect(article.valid?).to be false
+      end
     end
   end
 end
