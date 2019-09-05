8cfe13e2289805985a48e77f31046305768b6681
廣田真由 <my-hrt@i.softbank.jp>
2019-09-05 13:56:44 +0900

    articleモデル バリデーション追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,6 +13,12 @@
 #
 
 class Article < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :body
+  end
+
   belongs_to :user
   has_many :article_likes, dependent: :destroy
   has_many :comments, dependent: :destroy
