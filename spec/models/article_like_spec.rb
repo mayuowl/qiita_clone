a9962a7cbf63aa76274e1791fa7849ff9280af8a
廣田真由 <my-hrt@i.softbank.jp>
2019-10-04 16:46:12 +0900

    article_like model テスト実装

--- spec/models/article_like_spec.rb
+++ spec/models/article_like_spec.rb
@@ -14,5 +14,18 @@
 require "rails_helper"
 
 RSpec.describe ArticleLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "user_id と article_id がある時" do
+    it "いいねできる" do
+      article = create(:article)
+      article_like = ArticleLike.create(user_id: article.user_id, article_id: article.id)
+      expect(article_like).to be_valid
+    end
+  end
+  context "user_id と article_id がない時" do
+    it "いいねできない" do
+      article = create(:article)
+      article_like = ArticleLike.create(user_id: nil, article_id: nil)
+      expect(article_like.valid?).to be false
+    end
+  end
 end
