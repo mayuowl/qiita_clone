5df23365096fb30262d88ad0c459e88f8558bfeb
廣田真由 <my-hrt@i.softbank.jp>
2019-10-04 14:20:50 +0900

    current_user などの alias を設定

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,4 +3,9 @@
 class Api::V1::ApiController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
+
+  alias_method :current_user, :current_api_v1_user
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
+
 end
--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::ArticlesController < Api::V1::ApiController
-  before_action :authenticate_api_v1_user!, only: %i[create update destroy]
+  before_action :authenticate_user!, only: %i[create update destroy]
   before_action :set_article, only: %i[update destroy]
 
   def index
@@ -15,7 +15,7 @@ class Api::V1::ArticlesController < Api::V1::ApiController
   end
 
   def create
-    article = current_api_v1_user.articles.create!(article_params)
+    article = current_user.articles.create!(article_params)
     render json: article
   end
 
@@ -35,6 +35,6 @@ class Api::V1::ArticlesController < Api::V1::ApiController
   end
 
   def set_article
-    @article = current_api_v1_user.articles.find(params[:id])
+    @article = current_user.articles.find(params[:id])
   end
 end
