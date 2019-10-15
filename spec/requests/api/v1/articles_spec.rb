1cb4d65593074f5b484d338a49ca2887e5db8efa
廣田真由 <my-hrt@i.softbank.jp>
2019-10-15 14:56:20 +0900

    記事一覧API　更新日追加、テスト追加

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "Articles", type: :request do
       subject
       res = JSON.parse(response.body)
       expect(res.length).to eq 3
-      expect(res[0].keys).to eq ["id", "title", "user"]
+      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
       expect(response).to have_http_status(200)
     end
