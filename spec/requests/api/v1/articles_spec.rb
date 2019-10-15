0b7a115be0ccc42431042b2857b8a04e9be9a86d
廣田真由 <my-hrt@i.softbank.jp>
2019-10-15 21:53:25 +0900

    rubocop 実行

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
 
-    let!(:article1) { create(:article, updated_at: 1.days.ago) }
+    let!(:article1) { create(:article, updated_at: 1.day.ago) }
     let!(:article2) { create(:article, updated_at: 2.days.ago) }
     let!(:article3) { create(:article) }
 
@@ -14,8 +14,8 @@ RSpec.describe "Articles", type: :request do
       subject
       res = JSON.parse(response.body)
       expect(res.length).to eq 3
-      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
-      expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
+      expect(res[0].keys).to eq %w[id title updated_at user]
+      expect(res.map { |d| d["id"] }).to eq [article3.id, article1.id, article2.id]
       expect(response).to have_http_status(200)
     end
   end
