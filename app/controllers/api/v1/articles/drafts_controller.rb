782ab6be4b094b93102b3eaee2f2c0badf568606
廣田真由 <my-hrt@i.softbank.jp>
2019-10-19 19:43:26 +0900

    api_controller.rb 修正

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-class Api::V1::Articles::DraftsController < Api::V1::Articles::ApiController
+class Api::V1::Articles::DraftsController < Api::V1::ApiController
   before_action :authenticate_user!, only: %i[index show]
 
   def index
-    articles = Article.draft.order(updated_at: :desc)
+    articles = current_user.articles.draft.order(updated_at: :desc)
     render json: articles
   end
 
   def show
-    article = Article.draft.find(params[:id])
+    article = current_user.articles.draft.find(params[:id])
     render json: article
   end
 end
