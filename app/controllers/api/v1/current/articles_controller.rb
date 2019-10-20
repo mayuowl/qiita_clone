cacb373e866df2370fcd8e74a3d71b2047b02a70
廣田真由 <my-hrt@i.softbank.jp>
2019-10-20 20:42:22 +0900

    current/articles_controller 修正

--- app/controllers/api/v1/current/articles_controller.rb
+++ app/controllers/api/v1/current/articles_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Current::ArticlesController < Api::V1::ApiController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!
 
   def index
     articles = current_user.articles.publish.order(updated_at: :desc)
