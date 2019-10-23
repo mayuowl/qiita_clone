d11d21d3c4c89bc4fbff955ed8fed7eed8f85b08
廣田真由 <my-hrt@i.softbank.jp>
2019-10-23 14:39:36 +0900

    user_serializer 追加

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -2,5 +2,5 @@
 
 class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :updated_at, :status
-  belongs_to :user
+  belongs_to :user, serializer: Api::V1::UserSerializer
 end
