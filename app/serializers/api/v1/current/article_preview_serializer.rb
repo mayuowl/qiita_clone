8de1c34f77e792a067c4a1508efaa2b9890d760c
廣田真由 <my-hrt@i.softbank.jp>
2019-10-23 15:05:36 +0900

    rubocop 実行

--- app/serializers/api/v1/current/article_preview_serializer.rb
+++ app/serializers/api/v1/current/article_preview_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::Current::ArticlePreviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :created_at
   belongs_to :user, serializer: Api::V1::UserSerializer
