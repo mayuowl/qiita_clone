2b67c8ab9c82bcbd589fa7f9646aa5f55b145927
廣田真由 <my-hrt@i.softbank.jp>
2019-10-30 15:51:56 +0900

    controller private インデント修正

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -3,11 +3,11 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
-  def sign_up_params
-    params.permit(:account, :email, :password, :password_confirmation)
-  end
+    def sign_up_params
+      params.permit(:account, :email, :password, :password_confirmation)
+    end
 
-  def account_update_params
-    params.permit(:account, :email)
-  end
+    def account_update_params
+      params.permit(:account, :email)
+    end
 end
